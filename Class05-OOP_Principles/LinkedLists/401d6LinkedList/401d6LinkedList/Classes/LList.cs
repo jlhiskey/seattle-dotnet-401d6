2bec611a2f8ec00b751ebb48a41ccdf52e354d2f
Amanda Iverson <ajohnson764@gmail.com>
2019-01-14 17:05:08 -0800

    Add Code challenge 6

--- Class05-OOP_Principles/LinkedLists/401d6LinkedList/401d6LinkedList/Classes/LList.cs
+++ Class05-OOP_Principles/LinkedLists/401d6LinkedList/401d6LinkedList/Classes/LList.cs
@@ -38,7 +38,62 @@
 
         public void Print()
         {
-            // output to the console the linked list
+            if(Head != null)
+            {
+                Current = Head;
+
+                while (Current.Next != null)
+                {
+                    System.Console.Write($"{Current.Value} => ");
+                    Current = Current.Next;
+                }
+
+                System.Console.WriteLine($"{Current.Value} => Null");
+            }
+            else
+            {
+                System.Console.WriteLine("Your LL is empty");
+            }
+
+
+        }
+
+        public void Append(int value)
+        {
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+
+            Node node = new Node(value);
+
+            Current.Next = node;
+
+        }
+
+        public void InsertBefore(int value, int newValue)
+        {
+            Current = Head;
+
+            if (Current.Value == value)
+            {
+                Insert(newValue);
+                return;
+            }
+
+
+            while (Current.Next != null )
+            {
+                if (Current.Next.Value == value)
+                {
+                    Node node = new Node(newValue);
+                    node.Next = Current.Next;
+                    Current.Next = node;
+                    return;
+                }
+
+                Current = Current.Next;
+            }
         }
 
 
