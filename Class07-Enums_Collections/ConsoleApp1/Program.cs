d29078fe32dddcb2b8c72c102d60ac46c462a1f9
vinicio@codefellows.com <vinicio@codefellows.com>
2019-01-15 17:07:34 -0800

    Day 07

---
+++ Class07-Enums_Collections/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContainer<string> myContainer = new MyContainer<string>();
+            myContainer.Add("Gregor 1");
+            myContainer.Add("Gregor 2");
+            myContainer.Add("Gregor 3");
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor"); // Vinicio - RESIZE
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor");
+            myContainer.Add("Gregor");
+
+            // Vinicio - foreach (and foreach only) doesn't require you to implement
+            // IEnumerable<T>, but please do it!
+            foreach(string item in myContainer)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyContainer<string> myFancyContainer = new MyContainer<string>
+            {
+                "Wow",
+                "this is super fancy",
+                "to me at least..."
+            };
+
+            foreach(string item in myFancyContainer)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
