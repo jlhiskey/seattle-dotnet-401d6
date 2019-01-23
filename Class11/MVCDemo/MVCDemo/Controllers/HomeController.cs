550c185c87707327590e46e848dcfd4945c34d79
Amanda Iverson <ajohnson764@gmail.com>
2019-01-22 23:25:00 -0800

    class 11 mvc demo

---
+++ Class11/MVCDemo/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // returns the view in the Views >> Home >> Index.cshtml location
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName)
+        {
+            return RedirectToAction("Results", new { firstName, lastName });
+        }
+
+        public IActionResult Results(string firstName, string lastName)
+        {
+            Student student = new Student(firstName, lastName);
+            return View(student);
+        }
+    }
+}
---
+++ Class11/MVCDemo/MVCDemo/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
