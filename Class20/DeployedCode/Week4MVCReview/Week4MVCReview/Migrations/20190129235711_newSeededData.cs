9172bc1ca47a6e29b6c66aa0b8b1d996b41f2e81
Amanda Iverson <ajohnson764@gmail.com>
2019-02-02 11:38:31 -0800

    add deployed code

---
+++ Class20/DeployedCode/Week4MVCReview/Week4MVCReview/Migrations/20190129235711_newSeededData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Week4MVCReview.Migrations
+{
+    public partial class newSeededData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "ID", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Basket Weaving", 100 },
+                    { 2, "RacketBall", 50 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "ID", "Age", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, 50, "Barbie", "Doll" },
+                    { 2, 80, "Donald", "Duck" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LastName",
+                table: "Students",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ Class20/DeployedCode/Week4MVCReview/Week4MVCReview/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Week4MVCReview.Data;
+using Week4MVCReview.Models.Interfaces;
+using Week4MVCReview.Models.Services;
+
+namespace Week4MVCReview
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Declare our dependencies
+            services.AddMvc();
+
+            services.AddDbContext<DemoDbContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
+
+            services.AddScoped<ICourse, CourseService>();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
