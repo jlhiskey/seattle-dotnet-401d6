a1f25c39fbd3352073d72ee6aa72242b95dc2f74
Amanda Iverson <ajohnson764@gmail.com>
2019-02-19 17:07:43 -0800

    add Demo Code

---
+++ Class32/BlogPostCMS/BlogPostCMS/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using BlogPostCMS.Models;
+using BlogPostCMS.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlogPostCMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        /// <summary>
+        /// Default Register Page
+        /// </summary>
+        /// <returns>Return the View of the Register page</returns>
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create a new Application User
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = rvm.Email,
+                    Email = rvm.Email,
+                    FirstName = rvm.FirstName,
+                    LastName = rvm.LastName,
+                    Birthday = rvm.Birthday
+                };
+
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
+                   await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+
+                }
+            
+            }
+
+            return View(rvm);
+
+        }
+
+    }
+}
